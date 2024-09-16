dc6070a8b8ecac1dd71791ec8b2e0825a469e71a
mehrad <mmfirst@protonmail.com>
2024-09-16 20:42:27 +0330

    Implemented patch,delete,toggle

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"strings"
 
@@ -10,6 +9,11 @@ import (
 
 type header struct {
 	Authorization string `reqHeader:"Authorization"`
+}
+
+type updateTodo struct {
+	OldDescription string `json:"oldDescription"`
+	NewDescription string `json:"newDescription"`
 }
 
 var user string
@@ -40,7 +44,6 @@ func main() {
 		var user = c.Locals(user).(string)
 
 		db.Model(&Todo{}).Where("user = ?", user).Find(&todos)
-		fmt.Println(todos)
 
 		return c.JSON(todos)
 	})
@@ -58,7 +61,67 @@ func main() {
 		var user = c.Locals(user).(string)
 		db.Create(&Todo{User: user, Description: t.Description})
 
-		return c.JSON(fiber.Map{"status": "ok"})
+		return c.JSON(APITodo{t.Description, t.Done})
+	})
+
+	app.Delete("/", func(c *fiber.Ctx) error {
+		t := new(Todo)
+		err := c.BodyParser(t)
+
+		if err != nil || t.Description == "" {
+			return c.Status(fiber.ErrBadRequest.Code).JSON(fiber.Map{
+				"error": "please provide description in json",
+			})
+		}
+
+		var user = c.Locals(user).(string)
+		db.Where(&Todo{Description: t.Description, User: user}).Delete(&t)
+
+		return c.JSON(APITodo{t.Description, t.Done})
+	})
+
+	app.Patch("/", func(c *fiber.Ctx) error {
+		t := new(updateTodo)
+		err := c.BodyParser(t)
+
+		if err != nil || t.NewDescription == "" || t.OldDescription == "" {
+			return c.Status(fiber.ErrBadRequest.Code).JSON(fiber.Map{
+				"error": "please provide OldDescription,NewDescription in json",
+			})
+		}
+
+		var user = c.Locals(user).(string)
+
+		res := new(Todo)
+		db.Where(&Todo{Description: t.OldDescription, User: user}).Find(&res)
+
+		res.Description = t.NewDescription
+		db.Save(&res)
+		return c.JSON(APITodo{res.Description, res.Done})
+	})
+
+	app.Post("/toggle", func(c *fiber.Ctx) error {
+		t := new(Todo)
+		err := c.BodyParser(t)
+
+		if err != nil || t.Description == "" {
+			return c.Status(fiber.ErrBadRequest.Code).JSON(fiber.Map{
+				"error": "please provide OldDescription,NewDescription in json",
+			})
+		}
+
+		var user = c.Locals(user).(string)
+
+		res := db.Where(&Todo{Description: t.Description, User: user}).First(&t)
+		if res.Error != nil && res.Error.Error() == "record not found" {
+			return c.Status(fiber.ErrBadRequest.Code).JSON(fiber.Map{
+				"error": "No todo found with that Description",
+			})
+		}
+
+		t.Done = !t.Done
+		db.Save(&t)
+		return c.JSON(APITodo{t.Description, t.Done})
 	})
 
 	log.Fatal(app.Listen(":3000"))
